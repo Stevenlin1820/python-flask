12a85083e632acd90b71ad926b0c9bcc00823bef
Stevenlin1820 <104259197+Stevenlin1820@users.noreply.github.com>
2023-04-14 19:14:10 +0800

    Add files via upload

---
+++ py-flask/project-2.py
@@ -0,0 +1,32 @@
+from flask import Flask
+from flask import redirect, request
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello_sprout():
+    return "<h1>Hello, NLHS!</h1>"
+
+@app.route("/graduationthreshold")
+def lecturer():
+    return '<blockquote class="imgur-embed-pub" lang="en" data-id="a/XHyInsD" data-context="false" ><a href="//imgur.com/a/XHyInsD"></a></blockquote><script async src="//s.imgur.com/min/embed.js" charset="utf-8"></script>'
+
+@app.route("/price")
+def fee():
+    return "<p>兩個階段都1700</p>"
+
+
+@app.route("/student/<studentName>")
+def hello_student(studentName):
+    return f"<h1>Hello, {studentName}!</h1>"
+
+@app.route("/ive")
+def ive():
+    return redirect("https://www.youtube.com/watch?v=6ZUIwj3FgUY")
+
+@app.route("/schoolsong")
+def ivevideo():
+    return '<iframe width="967" height="544" src="https://www.youtube.com/embed/ypPW7847W0M" title="桃園市內壢高中校歌" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>'
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ py-flask/project-3.py
@@ -0,0 +1,50 @@
+from flask import Flask
+from flask import render_template, request
+import xlrd
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/myname")
+def jaySayHi():
+    return render_template("myname.html", chineseName="林嵩憲", englishName="Steven")
+
+
+@app.route("/agetest")
+def agetest():
+    age = 0
+    try:
+        age = int(request.args.get("age"))
+    except Exception as e:
+        print(e)
+    return render_template("age.html", age=age)
+
+
+@app.route("/store")
+def store():
+    cart = {"可樂": 12, "檸檬紅茶": 34, "咖啡": 101}
+    return render_template("store.html", cart=cart)
+
+
+@app.route("/ntu")
+def ntu():
+    return render_template("ntu.html")
+
+
+@app.route("/ntu/<departmentCode>")
+def department(departmentCode):
+    workbook = xlrd.open_workbook_xls("dpt_code.xls")
+    worksheet = workbook[0]
+    info = f"{departmentCode} 沒有這個科系"
+    for row, code in enumerate(worksheet.col(1)):
+        if (code.value == departmentCode):
+            info = f"{departmentCode}－{worksheet.cell_value(row , 2)} {worksheet.cell_value(row , 6)}"
+    return render_template("department.html", info=info)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
